0a4bc0086a125fbf1a06851d276c496c54a031a4
Jhonatan Castro <jhonatan.castro@cajamag.com.co>
2026-05-25 23:21:53 -0500

    fix(vercel): anadir serverExternalPackages (ahora si valido en Next 15) para que NFT incluya sequelize/pg/bcryptjs en el bundle de la Function

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverExternalPackages: ['sequelize', 'pg', 'pg-hstore', 'bcryptjs'],
 }
 
 module.exports = nextConfig
